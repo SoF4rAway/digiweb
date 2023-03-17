71bf23e27535060266855359d5a0da6fc29e4434
SoF4rAway <muhammadfaridrahman.mfr@gmail.com>
2023-03-17 19:44:50 +0700

    Digikom Website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
         <div class="col-md-4">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">Open Recruitment Asisten Magang</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="card-link">Card link</a>
