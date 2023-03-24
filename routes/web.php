41a5d63b5056bc83d8ac85e1f4040284388b1861
SoF4rAway <muhammadfaridrahman.mfr@gmail.com>
2023-03-24 11:53:13 +0700

    Admin Login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Home\HomePostController;
+use App\Http\Controllers\Home\homeController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AdminPostController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Admin\PostController;
-use App\Http\Controllers\homeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +17,31 @@ use App\Http\Controllers\homeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//Main Landing Page
-Route::get('/', [homeController::class, 'index'])->name('home');
-Route::get('/show',[homeController::class,'show'])->name('show');
 
-//Admin Route
-Route::resource('admin', AdminController::class);
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
-
-//Post Route
-Route::prefix('admin')->name('admin.')->group(function (){
-    Route::resource('posts',PostController::class);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::namespace('App\Http\Controllers\Home')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/posts/{post}', [HomePostController::class, 'show'])->name('home.posts.show');
+    // other home routes
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::resource('posts', AdminPostController::class);
+    Route::get('/', [AdminController::class, 'index'])->name('admin');
+    // other admin routes
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -23,7 +23,6 @@ return array(
     '35a6ad97d21e794e7e22a17d806652e4' => $vendorDir . '/nunomaduro/termwind/src/Functions.php',
     '09f6b20656683369174dd6fa83b7e5fb' => $vendorDir . '/symfony/polyfill-uuid/bootstrap.php',
     'a1105708a18b76903365ca1c4aa61b02' => $vendorDir . '/symfony/translation/Resources/functions.php',
-    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     '801c31d8ed748cfa537fa45402288c95' => $vendorDir . '/psy/psysh/src/functions.php',
     'e39a8b23c42d4e1452234d762b03835a' => $vendorDir . '/ramsey/uuid/src/functions.php',
     '265b4faa2b3a9766332744949e83bf97' => $vendorDir . '/laravel/framework/src/Illuminate/Collections/helpers.php',
@@ -31,6 +30,5 @@ return array(
     'f0906e6318348a765ffb6eb24e0d0938' => $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
     '58571171fd5812e6e447dce228f52f4d' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
     'a1cfe24d14977df6878b9bf804af2d1c' => $vendorDir . '/nunomaduro/collision/src/Adapters/Phpunit/Autoload.php',
-    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
     '320163ac6b93aebe3dc25b60a0533d56' => $vendorDir . '/spatie/laravel-ignition/src/helpers.php',
 );
